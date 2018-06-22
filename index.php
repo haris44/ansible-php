3e3ddc31885f7e1dfa202512f2e587c72b6ffbb1
Alexandre Bertrand <lxndr.bertrand@gmail.com>
2018-06-22 11:33:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test PHP</title>
+</head>
+<body>
+    <?php 
+        try {
+            $user = "ansible";
+            $pass = "ansible";
+            $dbh = new PDO('mysql:host=localhost:8899;dbname=ansible', $user, $pass);
+            foreach($dbh->query('SELECT * from hosts') as $row) {
+               var_dump($row);
+            }
+            $dbh = null;
+        } catch (PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    ?>
+</body>
+</html>
